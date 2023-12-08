e0e2cc3f6a41c653c7218ae8057073a3eec44324
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2023-12-08 19:40:57 -0300

    adiciona testes para AnaliseSuspeito

---
+++ btg-testes-auto/btg-test/AnaliseSuspeitosTest.cs
@@ -0,0 +1,59 @@
+﻿using btg_testes_auto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace btg_test
+{
+    public class AnaliseSuspeitosTest
+{
+        [Theory(DisplayName = "0 a 1 resposta positivas")]
+        [InlineData(true, false, false, false, false)]
+        [InlineData(false, false, false, false, false)]
+        [InlineData(false, false, true, false, false)]
+        public void AnaliseDoSuspeito_ZeroOuUmaRespostaPositiva_RetornaInocente(bool telefonouVitima, bool esteveNoLocal, bool moraPerto, bool devedor, bool trabalhaComVitima)
+        {
+            AnaliseSuspeitos analiseSuspeitos = new AnaliseSuspeitos();
+            string retorno = analiseSuspeitos.ExecutarQuestionarioSuspeito(telefonouVitima, esteveNoLocal, moraPerto, devedor, trabalhaComVitima);
+            
+            Assert.Equal("Inocente", retorno);
+        }
+
+        [Theory(DisplayName = "2 respostas positivas")]
+        [InlineData(true, true, false, false, false)]
+        [InlineData(false, true, true, false, false)]
+        [InlineData(false, false, true, false, true)]
+        public void AnaliseDoSuspeito_DuasRespostaPositiva_RetornaSuspeita(bool telefonouVitima, bool esteveNoLocal, bool moraPerto, bool devedor, bool trabalhaComVitima)
+        {
+            AnaliseSuspeitos analiseSuspeitos = new AnaliseSuspeitos();
+            string retorno = analiseSuspeitos.ExecutarQuestionarioSuspeito(telefonouVitima, esteveNoLocal, moraPerto, devedor, trabalhaComVitima);
+
+            Assert.Equal("Suspeita", retorno);
+        }
+
+        [Theory(DisplayName = "3 ou 4 respostas positivas")]
+        [InlineData(true, true, true, false, false)]
+        [InlineData(false, true, true, true, true)]
+        [InlineData(false, false, true, true, true)]
+        public void AnaliseDoSuspeito_TresOuQuatroRespostaPositiva_RetornaCumplice(bool telefonouVitima, bool esteveNoLocal, bool moraPerto, bool devedor, bool trabalhaComVitima)
+        {
+            AnaliseSuspeitos analiseSuspeitos = new AnaliseSuspeitos();
+            string retorno = analiseSuspeitos.ExecutarQuestionarioSuspeito(telefonouVitima, esteveNoLocal, moraPerto, devedor, trabalhaComVitima);
+
+            Assert.Equal("Cúmplice", retorno);
+        }
+
+        [Theory(DisplayName = "5 respostas positivas")]
+        [InlineData(true, true, true, true, true)]
+
+        public void AnaliseDoSuspeito_TresOuQuatroRespostaPositiva_RetornaAssassino(bool telefonouVitima, bool esteveNoLocal, bool moraPerto, bool devedor, bool trabalhaComVitima)
+        {
+            AnaliseSuspeitos analiseSuspeitos = new AnaliseSuspeitos();
+            string retorno = analiseSuspeitos.ExecutarQuestionarioSuspeito(telefonouVitima, esteveNoLocal, moraPerto, devedor, trabalhaComVitima);
+
+            Assert.Equal("Assassino", retorno);
+        }
+    }
+}
