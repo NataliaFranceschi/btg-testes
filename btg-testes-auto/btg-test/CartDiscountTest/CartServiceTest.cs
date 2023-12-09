e56dc9837a23da5863f6f52bb1a46ea2dd53fa7b
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2023-12-09 11:40:17 -0300

    adiciona testes CartDiscount

---
+++ btg-testes-auto/btg-test/CartDiscountTest/CartServiceTest.cs
@@ -0,0 +1,45 @@
+﻿using btg_testes_auto.CartDiscount;
+using btg_testes_auto.Order;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace btg_test.CartDiscountTest
+{
+    public class CartServiceTest
+    {
+        private readonly IDiscountService _mockDiscountService;
+        private readonly CartService _sut;
+
+        public CartServiceTest()
+        {
+            _mockDiscountService = Substitute.For<IDiscountService>();
+            _sut = new CartService(_mockDiscountService);
+        }
+
+        [Fact]
+        public void ProcessOrder_QuantityLowerThanStockUpdateSucess_ReturnTrue()
+        {
+            // Arrange
+            CartItem cartItem = new()
+            {
+                ProductId = "1",
+                Price = 150,
+            };
+
+            List<CartItem> cart = new() {  cartItem, cartItem };
+
+            _mockDiscountService.CalculateDiscount(cart)
+                .Returns(30);
+
+            double result = _sut.CalculateTotalWithDiscount(cart);
+
+            // Assert
+            result.Equals(270);
+            _mockDiscountService.Received().CalculateDiscount(cart);
+        }
+    }
+}
