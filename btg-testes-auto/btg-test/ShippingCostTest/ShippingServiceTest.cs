b7c70b8094eed4be5e0877691c27e092284d6f80
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2023-12-10 20:59:01 -0300

    adiciona testes ShippingService

---
+++ btg-testes-auto/btg-test/ShippingCostTest/ShippingServiceTest.cs
@@ -0,0 +1,72 @@
+﻿using btg_testes_auto.Discount;
+using btg_testes_auto.ShippingCost;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace btg_test.ShippingCostTest
+{
+    public class ShippingServiceTest
+    {
+        private readonly IDeliveryCostCalculator _mockDeliveryCostCalculator;
+        private readonly ShippingService _sut;
+
+        public ShippingServiceTest()
+        {
+            _mockDeliveryCostCalculator = Substitute.For<IDeliveryCostCalculator>();
+            _sut = new ShippingService(_mockDeliveryCostCalculator);
+        }
+
+        [Fact]
+        public void ProcessDeliveryCost_DistanceGreaterThan200DeliveryTypeExpress_Return50()
+        {
+            var cost = _mockDeliveryCostCalculator.CalculateCost(220, DeliveryType.Express)
+                .Returns(100);
+
+            double result = _sut.CalculateShippingCost(220, DeliveryType.Express);
+
+            result.Equals(50);
+            _mockDeliveryCostCalculator.Received().CalculateCost(220, DeliveryType.Express);
+        }
+
+        [Fact]
+        public void ProcessDeliveryCost_DistanceLessThan200DeliveryTypeExpress_Return100()
+        {
+            var cost = _mockDeliveryCostCalculator.CalculateCost(180, DeliveryType.Express)
+                .Returns(100);
+
+            double result = _sut.CalculateShippingCost(180, DeliveryType.Express);
+
+            result.Equals(100);
+            _mockDeliveryCostCalculator.Received().CalculateCost(180, DeliveryType.Express);
+        }
+
+        [Fact]
+        public void ProcessDeliveryCost_DistanceGreaterThan200DeliveryTypeOrdinary_Return100()
+        {
+            var cost = _mockDeliveryCostCalculator.CalculateCost(220, DeliveryType.Ordinary)
+                .Returns(100);
+
+            double result = _sut.CalculateShippingCost(220, DeliveryType.Ordinary);
+
+            result.Equals(100);
+            _mockDeliveryCostCalculator.Received().CalculateCost(220, DeliveryType.Ordinary);
+        }
+
+        [Fact]
+        public void ProcessDeliveryCost_DistanceLessThan200DeliveryTypeOrdinary_Return100()
+        {
+            var cost = _mockDeliveryCostCalculator.CalculateCost(220, DeliveryType.Ordinary)
+                .Returns(100);
+
+            double result = _sut.CalculateShippingCost(220, DeliveryType.Ordinary);
+
+            result.Equals(100);
+            _mockDeliveryCostCalculator.Received().CalculateCost(220, DeliveryType.Ordinary);
+        }
+    
+}
+}
