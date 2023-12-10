4d04709a6883846360237fd10599470dcc12885e
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2023-12-10 20:22:14 -0300

    adiciona testes PlaylistSong

---
+++ btg-testes-auto/btg-test/PlaylistSongTest/PlaylistServiceTest.cs
@@ -0,0 +1,103 @@
+﻿using btg_testes_auto.PlaylistSongs;
+using FluentAssertions;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace btg_test.PlaylistSongTest
+{
+    public class PlaylistServiceTest
+    {
+        private readonly IPlaylistValidationService _mockPlaylistValidationService;
+        private PlaylistService _sut; 
+
+        public PlaylistServiceTest()
+        {
+            _mockPlaylistValidationService = Substitute.For<IPlaylistValidationService>();
+            _sut = new(_mockPlaylistValidationService);
+        }
+
+        Song song1 = new Song() { Artist = "artist1", Title = "title1" };
+        Song song2 = new Song() { Artist = "artist2", Title = "title2" };
+        Song song3 = new Song() { Artist = "artist3", Title = "title3" };
+
+        [Fact]
+        public void ProcessPlaylistValidation_AddSongToPlaylistWithSpace_ReturnTrue()
+        {
+            List<Song> list = new List<Song>() { song1, song2 };
+            Playlist playlist = new Playlist() { MaxSongs = 3, Songs = list };
+
+            _mockPlaylistValidationService.CanAddSongToPlaylist(playlist, song3)
+                .Returns(true);
+
+            bool result = _sut.AddSongToPlaylist(playlist, song3);
+
+            result.Should().BeTrue();
+            _mockPlaylistValidationService.Received().CanAddSongToPlaylist(playlist, song3);
+            playlist.Songs.Should().Contain(song1);
+            playlist.Songs.Should().Contain(song2);
+            playlist.Songs.Should().Contain(song3);
+        }
+
+        [Fact]
+        public void ProcessPlaylistValidation_AddSongFullPlaylist_ReturnFalse()
+        {
+            List<Song> list = new List<Song>() { song1, song2 };
+            Playlist playlist = new Playlist() { MaxSongs = 2, Songs = list };
+
+            _mockPlaylistValidationService.CanAddSongToPlaylist(playlist, song3)
+                .Returns(false);
+
+            bool result = _sut.AddSongToPlaylist(playlist, song3);
+
+            result.Should().BeFalse();
+            _mockPlaylistValidationService.Received().CanAddSongToPlaylist(playlist, song3);
+            playlist.Songs.Should().Contain(song1);
+            playlist.Songs.Should().Contain(song2);
+            playlist.Songs.Should().NotContain(song3);
+        }
+
+        [Fact]
+        public void ProcessPlaylistValidation_AddSongsToPlaylistWithSpace_ReturnTrue()
+        {
+            List<Song> songs = new List<Song>() { song1, song2 };
+
+            Playlist playlist = new Playlist() { MaxSongs = 3, Songs = new List<Song>() };
+
+            _mockPlaylistValidationService.CanAddSongToPlaylist(playlist, Arg.Any<Song>())
+                .Returns(true);
+
+            _sut.AddSongsToPlaylist(playlist, songs);
+
+            _mockPlaylistValidationService.Received(2).CanAddSongToPlaylist(playlist, Arg.Any<Song>());
+            playlist.Songs.Should().Contain(song1);
+            playlist.Songs.Should().Contain(song2);
+        }
+
+        [Fact]
+        public void ProcessPlaylistValidation_AddSongsFullPlaylist_ReturnFalse()
+        {
+            List<Song> songs = new List<Song>() { song1, song2, song3 };
+
+            Playlist playlist = new Playlist() { MaxSongs = 2, Songs = new List<Song>() };
+
+            _mockPlaylistValidationService.CanAddSongToPlaylist(playlist, song1)
+                .Returns(true);
+            _mockPlaylistValidationService.CanAddSongToPlaylist(playlist, song2)
+                .Returns(true);
+            _mockPlaylistValidationService.CanAddSongToPlaylist(playlist, song3)
+                .Returns(false);
+
+            _sut.AddSongsToPlaylist(playlist, songs);
+
+            _mockPlaylistValidationService.Received(3).CanAddSongToPlaylist(playlist, Arg.Any<Song>());
+            playlist.Songs.Should().Contain(song1);
+            playlist.Songs.Should().Contain(song2);
+            playlist.Songs.Should().NotContain(song3);
+        }
+
+    }
+}
